90945f321a50ce0b7b2213fc5a95608c41f835fa
MaksymTovstolis <85698971+MaksymTovstolis@users.noreply.github.com>
2021-09-23 13:28:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,52 +3,23 @@ let native = ['York','Of'];
 let destination = ['Poltava','In'];
 let rainbow;
 
-rainbow = hero.concat(native,destination);
-rainbow.reverse();
-
-// [ "In", "Poltava", "Of", "York", "Ivan" ]
-// ['Richard','Of','York','Gave','Battle','In','Vain'];
-
-rainbow.push("Gave");
-rainbow.unshift("Richard");
-rainbow.splice(2,1,"Battle");
+rainbow = hero.concat(native.reverse(),destination.reverse());
+rainbow[0] = `Richard`;
+rainbow[rainbow.length-1] = `Vain`;
+rainbow.splice(3,0,'Gave','Battle');
 
 console.log(rainbow);
-console.log(`'Richard','Of','York','Gave','Battle','In','Vain'`)
 
-if (rainbow[1] = 'In') {
-    rainbow[1] = 'Of'
-    rainbow[5] = 'In';
-}
-if(rainbow[2] = `Battle`) {
-    rainbow[2] = 'York'
-    rainbow[4] = 'Battle'
-    rainbow[3] = 'Gave'
-    rainbow[6] = 'Vain'
-}
+let colors = [`red`,`orange`,`yellow`,`green`,`blue`,`indigo`,`violet`];
 
-document.write(`
-<table>
-
+debugger;
+for(let i=0; i<rainbow.length; i++){
+	document.write (`
     <tr>
-        <th style='background: violet; border-radius: 100%; width: 50px; height: 50px;'> </th>
-        <th style='background: orange; border-radius: 100%; width: 50px; height: 50px;'> </th>
-        <th style='background: yellow; border-radius: 100%; width: 50px; height: 50px;'> </th>
-        <th style='background: green; border-radius: 100%; width: 50px; height: 50px;'> </th>
-        <th style='background: blue; border-radius: 100%; width: 50px; height: 50px;'> </th>
-        <th style='background: darkblue; border-radius: 100%; width: 50px; height: 50px;'> </th>
-        <th style='background: violet; border-radius: 100%; width: 50px; height: 50px;'> </th>
+        <th style= background: ${colors[i]}; border-radius: 100%; width: 50px; height: 50px;> </th>   
     </tr>
     <tr>
-    <th>${rainbow[0]}</th>
-    <th>${rainbow[1]}</th>
-    <th>${rainbow[2]}</th>
-    <th>${rainbow[3]}</th>
-    <th>${rainbow[4]}</th>
-    <th>${rainbow[5]}</th>
-    <th>${rainbow[6]}</th>
-</tr>
-</table>`)
-
-
-console.log(rainbow);
+        <th>${rainbow[i]}</th>
+    </tr>
+    `)
+}
